b55b43e37ddf342171da3a1dbf0745cfdc5fef00
stacy perales <stacy.perales@ynov.com>
2021-01-29 14:56:54 +0100

    listeConcert avec /event/search

--- src/compnent/administrator/listConcert.js
+++ src/compnent/administrator/listConcert.js
@@ -9,11 +9,15 @@ import './listConcert.css';
 
 function AdminConcertList() {
 
-    //react Hook useState
-    let [concerts, setConcerts] = useState([])
+    //react Hook useState map : pour les array pas JSON
+    let [allConcerts, setConcerts] = useState({
+        events: []
+    })
     //react Hook useEffect
     useEffect(() => {
-        axios.get('http://127.0.0.1:8000/concert')
+        // axios.get('http://127.0.0.1:8000/concert')
+        axios.get('http://127.0.0.1:8000/event/search')
+
             .then(response => {
                 console.log(response.data);
                 setConcerts(response.data);
@@ -35,25 +39,32 @@ function AdminConcertList() {
                 <thead>
                     <tr>
                         <td>Id</td>
-                        <td>Nom</td>
-                        {/* <td>Artiste</td> */}
-                        <td>Artiste</td>
+                        <td>Nom de l'artiste /Groupe</td>
+                        <td>Nom du concert</td>
+                        <td>Date et heure</td>
                         <td>Lieu</td>
-                        <td>Date</td>
-                        <td>etc</td>
+                        <td>Catégorie de musique</td>
+                        <td>Catégorie de tarif</td>
+                        <td>Heure d'ouverture</td>
+                        <td>Présentation de l'article</td>
+                        <td>Player audio</td>
+
                     </tr>
                 </thead>
                 <tbody>
-                    {concerts.map(concert => (
+                    {allConcerts.events.map(concert => (
                         //L'attribut key est obligatoire pour le dataBinding
                         <tr key={concert.id}>
                             <td>{concert.id}</td>
-                            <td>{concert.event.name}</td>
-                            <td>{concert.priceMax}</td>
-                            <td>{concert.categoryNumber}</td>
-                            <td>{concert.date}</td>
-                            <td>{concert.event.name}</td>
-
+                            <td>{concert.artistName}</td>
+                            <td>Nom du concert</td>
+                            <td>{concert.concerts[0].date}{concert.concerts[0].time}</td>
+                            <td>{concert.salle.city}</td>
+                            <td>catégorie de musique</td>
+                            <td>{concert.concerts[0].time}</td>
+                            <td>{concert.concerts[0].openingTime}</td>
+                            <td>Présentation de l'article</td>
+                            <td>Player audio</td>
                         </tr>
                     ))}
                 </tbody>
